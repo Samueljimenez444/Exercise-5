13b0b99a92f79d9f282f6433938d0e24ef85d837
samuel.jimenez <samuel.jimenez@106-15.info2>
2024-09-20 14:04:04 +0200

    Exercise 5 Completed

---
+++ Exercise5/src/Exercise5/Exercise5.java
@@ -0,0 +1,11 @@
+package Exercise5;
+
+public class Exercise5 {
+
+	public static void main(String[] args) {
+		System.out.println("Name:Samuel Jimenez");
+		System.out.println("Address: Brenes Calle Victor Torres N*8");
+		System.out.println("Telephone Number: 611463589");
+	}
+
+}
